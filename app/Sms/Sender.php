1f52cfebb87d6790cc241b626ca177bcb0d8b5f6
ChengKaiChiang <mei80217@gmail.com>
2024-08-22 15:15:20 +0800

    add legacy sms

---
+++ app/Sms/Sender.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Sms;
+
+use Corp104\Common\Sms\Sms;
+use Illuminate\Contracts\View\View;
+
+/**
+ * 簡訊寄送器
+ */
+class Sender
+{
+    public function __construct(private readonly Sms $sms)
+    {
+    }
+
+    /**
+     * 寄送簡訊
+     */
+    public function send(string $cellphone, View $view): void
+    {
+        $content = trim($view->render());
+
+        // 公司舊版寄簡訊 lib
+        // sendTo() 方法實際是打舊版寄簡訊 api
+        $this->sms->sendTo($cellphone, $content);
+    }
+}
